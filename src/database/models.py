63c2fcc49fd9d87bbe0a26aca127122b56ef4799
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-12-30 21:52:19 +0100

    Update models
    - Added new dependencies to maintain a more accurate and efficient project setup.

--- src/database/models.py
+++ src/database/models.py
@@ -11,8 +11,9 @@ class Base(DeclarativeBase):
 association_table = Table(
     "user_likes",
     Base.metadata,
-    Column("user_id", UUID(as_uuid=True), ForeignKey("user.id")),
-    Column("post_id", Integer, ForeignKey("posts.id")),
+    Column("user_id", UUID(as_uuid=True), ForeignKey("user.id", ondelete="CASCADE")),
+    Column("post_id", Integer, ForeignKey("posts.id", ondelete="CASCADE")),
+    Column("created_at", DateTime, default=func.now())
 )
 
 
@@ -20,16 +21,20 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
     __table_args__ = {"extend_existing": True}
     username = Column(String(50), nullable=False)
     created_at = Column(DateTime, default=func.now())
-    posts = relationship("Post", back_populates="owner", lazy="joined")
-    liked_posts = relationship("Post", secondary=association_table, back_populates="likers")
+    posts = relationship("Post", back_populates="owner", cascade="all, delete", lazy="joined")
+    liked_posts = relationship("Post", secondary=association_table, back_populates="likers", cascade="all, delete",
+                               lazy="selectin")
+    last_login = Column(DateTime, default=None)
+    last_request_time = Column(DateTime, default=None)
 
 
 class Post(Base):
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True)
-    owner_id = Column(UUID(as_uuid=True), ForeignKey("user.id"))
+    owner_id = Column(UUID(as_uuid=True), ForeignKey("user.id", ondelete="CASCADE"))
     text = Column(String(555), nullable=True, default=None)
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=None, onupdate=func.now(), nullable=True)
-    owner = relationship("User", back_populates="posts", lazy="noload")
-    likers = relationship("User", secondary=association_table, back_populates="liked_posts")
+    owner = relationship("User", back_populates="posts", lazy="noload", cascade="all, delete")
+    likers = relationship("User", secondary=association_table, back_populates="liked_posts", lazy="selectin", cascade="all, delete")
+
